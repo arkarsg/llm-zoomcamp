6fbd0ed0418c3bbf4e7d80fbaed5829bb3e032d9
arkarsg <alxarkar@gmail.com>
2026-06-22 23:40:12 +0800

    lesson-02

---
+++ libs/rag_vector.py
@@ -0,0 +1,17 @@
+from libs.rag import RAGBase
+
+
+class RAGVector(RAGBase):
+    def __init__(self, embedder, **kwargs):
+        super().__init__(**kwargs)
+        self.embedder = embedder
+
+    def search(self, query, num_results=5):
+        query_vector = self.embedder.encode(query)
+        filter_dict = {"course": self.course}
+
+        return self.index.search(
+            query_vector,
+            num_results=num_results,
+            filter_dict=filter_dict
+        )
